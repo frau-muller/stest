f761464998109b152976b988d4b061a19a16cf64
frau-muller <frau-muller@tut.by>
2021-11-21 22:15:55 +0300

    2

--- django/core/settings.py
+++ django/core/settings.py
@@ -116,9 +116,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 
 CORS_ALLOWED_ORIGINS = [
