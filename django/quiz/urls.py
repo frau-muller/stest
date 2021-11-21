14c426f019741607ebb5a511cdd844722c6a7435
frau-muller <frau-muller@tut.by>
2021-11-21 22:00:37 +0300

    1

--- django/quiz/urls.py
+++ django/quiz/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import Quiz, RandomQuestionTopic, StartQuiz
 from django.conf import settings
-#from django.conf.urls.static import static
+from django.conf.urls.static import static
 
 app_name='quiz'
 
@@ -11,4 +11,4 @@ urlpatterns = [
     #path('a/', Answer.as_view(), name='answer'),
     path('r/<str:topic>/', RandomQuestionTopic.as_view(), name='RandomQuestionTopic'),
     path('single/<str:title>/', StartQuiz.as_view(), name='quiz'),
-] + static(settings.STATICcd_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
