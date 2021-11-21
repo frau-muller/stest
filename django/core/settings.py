57b6f7e209d7f74f4aa9554fc3ea4cac3e9adc37
frau-muller <frau-muller@tut.by>
2021-11-21 17:29:17 +0300

    1

--- django/core/settings.py
+++ django/core/settings.py
@@ -110,13 +110,13 @@ USE_TZ = True
 #Static files (CSS, JavaScript, Images)
 #https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
+#STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 CORS_ALLOWED_ORIGINS = [
